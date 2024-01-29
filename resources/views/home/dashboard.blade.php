9e7774bdb92c781fe1dbe6642e1a41342b74879b
JC <cjoaquinangel@gmail.com>
2024-01-29 12:24:05 -0600

    Agregado y funcional componente de vue

--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -8,8 +8,10 @@
     <div class="card w-100">
         <h1>Bienvenido, User</h1>
         <br>
-        <div>
-            
+        <div class="card-body">
+            <div id="app">
+                <example-component></example-component>
+            </div>
         </div>
     </div>
 
